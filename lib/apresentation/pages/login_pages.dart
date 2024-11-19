90ed44c70e79ccb21aa99176d1a88a3d0a5909c3
Quiabo_Vegano <155786316+Guilherme-Cardozo-688@users.noreply.github.com>
2024-11-19 19:25:31 -0300

    :sparkles: feat(login): Transforma a tela de login em um StatefulWidget, adiciona validação de formulário e controle de visibilidade da senha.

--- lib/apresentation/pages/login_pages.dart
+++ lib/apresentation/pages/login_pages.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:trouve_moi_mobile/apresentation/standart/app_colors.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _passwordVisible = false;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      print('Email: ${emailController.text}');
+      print('Senha: ${passwordController.text}');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    TextEditingController? emailController;
-    TextEditingController? passwordController;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.fundoLogo,
@@ -20,12 +41,12 @@ class LoginScreen extends StatelessWidget {
           Container(
             color: AppColors.background,
           ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
           Positioned(
-              width: MediaQuery.of(context).size.width,
-              child: const Image(
-                image: AssetImage('assets/images/LOGIN.png'),
-              )),
+            width: MediaQuery.of(context).size.width,
+            child: const Image(
+              image: AssetImage('assets/images/LOGIN.png'),
+            ),
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
@@ -34,64 +55,90 @@ class LoginScreen extends StatelessWidget {
               decoration: const BoxDecoration(
                 color: AppColors.background,
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                      prefixIcon: Icon(
-                        Icons.person,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        prefixIcon: Icon(Icons.person),
                       ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'O campo username é obrigatório';
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.visibility),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: IconButton(
+                          icon: Icon(
+                            _passwordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _passwordVisible = !_passwordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                      obscureText: !_passwordVisible,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'O campo senha é obrigatório';
+                        }
+                        return null;
+                      },
                     ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.buttonVerde,
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                    ),
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _submitForm,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.buttonVerde,
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
                           color: AppColors.background,
                           fontFamily: GoogleFonts.tinos().fontFamily,
-                          fontSize: 18),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text.rich(
-                        TextSpan(
-                          text: 'Not a member? ',
-                          style: TextStyle(color: Colors.black54),
-                          children: [
-                            TextSpan(
-                              text: 'Sign up',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ],
+                          fontSize: 18,
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text.rich(
+                          TextSpan(
+                            text: 'Not a member? ',
+                            style: TextStyle(color: Colors.black54),
+                            children: [
+                              TextSpan(
+                                text: 'Sign up',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
